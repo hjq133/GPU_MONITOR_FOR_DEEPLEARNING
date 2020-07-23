27fb927094037968144900564b2568aa2057d6a5
hjq133 <1399615054@qq.com>
2020-07-23 10:31:27 +0800

    publish gpu monitor version 2.0

--- monitor.py
+++ monitor.py
@@ -2,56 +2,81 @@ import pynvml
 import threading
 import os
 import time
+import argparse
+
+parser = argparse.ArgumentParser(description='GPU WATCHER')
+parser.add_argument('--gpu_ids', type=str, default='0,1,2,3',
+                    help='The id of the gpu to be monitored, separated by comma')
+parser.add_argument('--proportion', type=str, default=8, help='Occupancy ratio threshold')
+parser.add_argument('--time_threshold', type=int, default=600, help='Time threshold in seconds')
+parser.add_argument('--command', type=str, default='python train_search.py --gpu=',  # we will add gpu automatically
+                    help='Run your program with the corresponding gpu')
+parser.add_argument('--ratio', type=float, default=1024 ** 2)
+args = parser.parse_args()
 
 pynvml.nvmlInit()
-gpu_ids = [0, 1, 2, 3]  # gpu id
-gpu_handle = [pynvml.nvmlDeviceGetHandleByIndex(id) for _, id in enumerate(gpu_ids)]
-meminfo = [0 for _, id in enumerate(gpu_ids)]
-total = [0 for _, id in enumerate(gpu_ids)]
-used = [0 for _, id in enumerate(gpu_ids)]
-free = [0 for _, id in enumerate(gpu_ids)]
-ratio = 1024 ** 2
-proportion = 8.0
+gpu_ids = list(map(int, args.gpu_ids.split(',')))
+is_over = False  # Flag, if there exists a thread which is running your program, set is_over to True
 
 
-# class GpuMonitor(threading.Thread):  # 继承父类threading.Thread
-#     def __init__(self, gpu_id):
-#         threading.Thread.__init__(self)
-#         self.gpu_id = gpu_id
-#
-#     def run(self):  # 把要执行的代码写到run函数里面 线程在创建后会直接运行run函数
-#         os.system('python train.py')
-#         print("finish")
+class GpuMonitor(threading.Thread):
+    def __init__(self, gpu_id):
+        threading.Thread.__init__(self)
+        self.time_threshold = args.time_threshold
+        self.proportion = args.proportion
+        self.ratio = args.ratio
+        self.command = args.command + str(self.gpu_id)  # we will add gpu automatically
 
-def monitor(gpu_id, handle):  # 监测gpu是否已经长时间无人使用
-    for i in range(600):  # 循环10分钟
-        meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
-        total = meminfo.total / ratio  # 以兆M为单位就需要除以1024**2
-        used = meminfo.used / ratio
-        if used > total / proportion:
+        self.gpu_id = gpu_id
+        self.handle = pynvml.nvmlDeviceGetHandleByIndex(id)
+        self.memory_info = pynvml.nvmlDeviceGetMemoryInfo(self.handle)
+        self.total = self.memory_info.total / self.ratio
+        self.used = self.memory_info.used / self.ratio
+        self.free = self.memory_info.used / self.ratio
+
+    def run(self):  # running code
+        if self.used < self.total / self.proportion:  # if we already have empty gpu
+            if is_over:
+                return
+            self.run_program()
+        else:
+            print('gpu {} is being used, we are monitoring ...')
+            while True:
+                self.update_info()
+                if self.used < self.total / self.proportion:
+                    self.monitor()
+                time.sleep(5)  # sleep for 5 seconds
+
+    def update_info(self):
+        self.memory_info = pynvml.nvmlDeviceGetMemoryInfo(self.handle)
+        self.total = self.memory_info.total / self.ratio
+        self.used = self.memory_info.used / self.ratio
+        self.free = self.memory_info.used / self.ratio
+
+    def run_program(self):  # grabbed the gpu and run your program
+        print('We successfully grabbed the gpu {}'.format(self.gpu_id))
+        print('start run your program')
+        global is_over
+        is_over = True
+        os.system(self.command)
+        print('finish!')
+        exit(0)
+
+    def monitor(self):  # If no one is using it within 10 minutes, we will preempt the GPU
+        for i in range(self.time_threshold):
+            if self.used > self.total / self.proportion:
+                return
+            time.sleep(1)
+        if is_over:
             return
-        time.sleep(1)
-    print("now grabbed the gpu{}".format(gpu_id))
-    command = "python train_search.py --gpu={}".format(gpu_id)
-    os.system(command)
-    print("now grabbed the gpu{} finish!".format(gpu_id))
-    exit()
+        else:
+            self.run_program()
 
 
 def main():
-    while 1:
-        for i, handle in enumerate(gpu_handle):
-            meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
-            total = meminfo.total / ratio  # 以兆M为单位就需要除以1024**2
-            used = meminfo.used / ratio
-            free = meminfo.free / ratio
-            print("gpu id:{} | total :{} | used: {} | free: {}".format(gpu_ids[i], total, used, free))
-            if used < total / proportion:
-                print("start monitor gpu : {}".format(gpu_ids[i]))
-                monitor(gpu_ids[i], handle)
-                break
-        print("-" * 89)
-        time.sleep(1)  # 每隔1s监测一次gpu的状况
+    for _, gpu_id in enumerate(gpu_ids):
+        GpuMonitor(args, gpu_id).start()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
